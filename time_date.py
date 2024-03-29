ba0c190905999b0e21d05897e3d432397cd305df
73272dhdss <156158544+73272dhdss@users.noreply.github.com>
2024-03-29 14:28:32 +0530

    Update time_date.py

--- time_date.py
+++ time_date.py
@@ -1,3 +1,8 @@
+
+# `from datetime import datetime` is a Python statement that imports the `datetime` class from the
+# `datetime` module. This allows you to work with date and time objects in your Python code. By
+# importing `datetime` in this way, you can directly access the `datetime` class and its methods
+# without having to prefix them with the module name.
 # current time and date
 print("the sum of the numbers",sum)
 from datetime import datetime
