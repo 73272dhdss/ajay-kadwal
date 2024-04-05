19e747ac14208f81898b0c6437c044227fc012c7
73272dhdss <156158544+73272dhdss@users.noreply.github.com>
2024-04-05 14:26:42 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,7 @@
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+n3 = int(input("Enter the third number: "))
+
+largest_num = max(n1, n2, n3)
+print("The largest number is: ", largest_num)
